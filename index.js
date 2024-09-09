6017e04cfce279995d61aa42d30e68a716c8345f
Nyaaku Opeyemi Isaiah <nyaakuopeyemi007@gmail.com>
2024-09-09 10:44:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function showhoverbar(){
+        const hoverbar= document.querySelector('.hoverbar')
+        hoverbar.style.display = 'flex'        
+}
+function closehoverbar(){
+        const hoverbar= document.querySelector('.hoverbar')
+        hoverbar.style.display = 'none'
+}
